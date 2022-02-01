b525def594e0f1a1c5073c19d8e01f16aac13d8b
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-02-01 01:12:32 -0500

    clicker

---
+++ tests/click.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time
+
+w = 2560
+h = 1440
+
+print('Count down:')
+for i in range(3):
+    print(f'{3-i}...')
+    time.sleep(1)
+pyautogui.moveTo(w/2,h/2)
+
+for i in range(10):
+    pyautogui.click(clicks=1, interval=0.01)
+
+print('\nDown...')
+print(pyautogui.size())
